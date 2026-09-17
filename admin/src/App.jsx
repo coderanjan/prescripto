3803a84562a5a632286c4608de663396e65facc0
anjan pajiyar <anjanpanjiyar90@gmail.com>
2026-09-17 09:18:27 +0545

    first commit

---
+++ admin/src/App.jsx
@@ -0,0 +1,51 @@
+import React, { useContext } from 'react'
+import Login from './pages/Login'
+ import { ToastContainer, toast } from 'react-toastify'
+import { AdminContext } from './context/AdminContext'
+import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
+import Dashboard from './pages/Admin/Dashboard'
+import AllAppointment from './pages/Admin/AllAppointment'
+import AddDoctor from './pages/Admin/AddDoctor'
+import DoctorList from './pages/Admin/DoctorList'
+import { Route, Routes } from 'react-router-dom'
+import { DoctorContext } from './context/DoctorContext'
+import DoctorDashboard from './pages/Doctor/DoctorDashboard'
+import DoctorAppointments from './pages/Doctor/DoctorAppointments'
+import DoctorProfile from './pages/Doctor/DoctorProfile'
+
+
+
+const App = () => {
+  const {aToken} = useContext(AdminContext)
+  const {dToken} = useContext(DoctorContext)
+  return aToken || dToken? (
+    <div className='bg-[#F8F9FD]'>
+      <ToastContainer/>
+      <Navbar/>
+      <div className='flex items-start'>
+        <Sidebar/>
+        <Routes>
+          {/* Admin Route */}
+            <Route path='/' element={<></>} />
+            <Route path='/admin-Dashboard' element={<Dashboard />} />
+            <Route path='/all-appointments' element={<AllAppointment />} />
+            <Route path='/add-doctor' element={<AddDoctor />} />
+            <Route path='/doctor-list' element={<DoctorList />} />
+
+            {/* Doctor Route */}
+            <Route path='/doctor-Dashboard' element={<DoctorDashboard />} />
+            <Route path='/doctor-appointments' element={<DoctorAppointments />} />
+            <Route path='/doctor-profile' element={<DoctorProfile />} />
+        </Routes>
+      </div>
+    </div>
+  ):(
+    <>
+      <Login/>
+      <ToastContainer/>
+    </>
+  )
+}
+
+export default App
