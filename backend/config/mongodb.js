1e063275dcef74f64177407fed6348b584a2323c
anjan pajiyar <anjanpanjiyar90@gmail.com>
2026-09-17 09:42:32 +0545

    bug solved

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 const connectDB = async ()=>{
   mongoose.connection.on('connected',()=> console.log("database connected")
   )
-  await mongoose.connect(`${process.env.MONGODB_URl}/prescripto`)
+  await mongoose.connect(`${process.env.MONGODB_URl}`)
 }
 
 export default connectDB
